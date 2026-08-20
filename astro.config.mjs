8bbc6baf37d314f362b8e572416893915642cf24
arnobl <1052622+arnobl@users.noreply.github.com>
2026-08-20 15:03:22 +0200

    gh action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://khora-team.github.io',
     i18n: {
         locales: ["en"],
         defaultLocale: "en",
