19fd7ed3239c7f62d6ef97bc9d4bac042bd27c2c
arnobl <1052622+arnobl@users.noreply.github.com>
2026-08-20 16:30:53 +0200

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
